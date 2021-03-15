efc98a8645bd6da23a8247cc7141b5258f3da6b0
Belabessi Adam <belabessi.adam@gmail.com>
2021-03-15 00:18:30 +0000

    Turn a Class into Entity

--- src/main/java/com/corigo/pma/entities/Project.java
+++ src/main/java/com/corigo/pma/entities/Project.java
@@ -1,5 +1,48 @@
 package com.corigo.pma.entities;
 
 public class Project {
+	//Field
+	private long projectId ;
+	private String name ;
+	private String stage ;
+	private String description ;
+	
+	//Constructor
+	public Project() {}
+	public Project(String name, String stage, String description) {
+		super();
+		this.name = name;
+		this.stage = stage;
+		this.description = description;
+	}
+	//Getters and Setters
+	
+	public long getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(long projectId) {
+		this.projectId = projectId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStage() {
+		return stage;
+	}
+	public void setStage(String stage) {
+		this.stage = stage;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	} 
+	
+	
+	
 
 }
