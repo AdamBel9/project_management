ac778cab02f9f741dbc06f376590f374b5a56790
Belabessi Adam <belabessi.adam@gmail.com>
2021-03-16 22:01:09 +0000

    finalize the Project POJO

--- src/main/java/com/corigo/pma/entities/Project.java
+++ src/main/java/com/corigo/pma/entities/Project.java
@@ -1,7 +1,15 @@
 package com.corigo.pma.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Project {
 	//Field
+	@Id 
+	@GeneratedValue (strategy = GenerationType.AUTO)
 	private long projectId ;
 	private String name ;
 	private String stage ;
