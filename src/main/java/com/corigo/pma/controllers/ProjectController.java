9fb24ce24cfe609053cafdb2ff0c58db7adfedca
Belabessi Adam <belabessi.adam@gmail.com>
2021-03-17 21:53:02 +0000

    Create ProjectRepository

--- src/main/java/com/corigo/pma/controllers/ProjectController.java
+++ src/main/java/com/corigo/pma/controllers/ProjectController.java
@@ -1,19 +1,34 @@
 package com.corigo.pma.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.corigo.pma.deo.ProjectRepository;
 import com.corigo.pma.entities.Project;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+	
+	@Autowired
+	ProjectRepository proRep ;
 
-	@RequestMapping("/new")
+	@GetMapping("/new")
 	public String displayProjectForm(Model model) {
 		model.addAttribute("project", new Project());
 		return "new-project";
 	}
+	
+	@PostMapping("/save")
+	public String createProject(Project project , Model model) {
+		proRep.save(project );
+		//use redirect to prevent duplicate submissions 
+		return "redirect:/projects/new" ;
+	}
+	
 	
 }
---
+++ src/main/java/com/corigo/pma/deo/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.corigo.pma.deo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.corigo.pma.entities.Project;
+
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+}
